cadffc30fdaa14e62729f1949850ddcb77cd2e05
carvin <carson_192@163.com>
2021-01-21 16:16:18 +0800

    add mongoDB Scaler test image (#7)
    Co-authored-by: 高强 <wb-gq555900@alibaba-inc.com>

---
+++ e2e/images/mongodb/cmd/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+	"log"
+)
+
+var (
+	connectStr string
+	dataBase   string
+	collection string
+)
+
+func main() {
+	flag.StringVar(&connectStr, "connectStr", "", "")
+	flag.StringVar(&dataBase, "dataBase", "", "")
+	flag.StringVar(&collection, "collection", "", "")
+	flag.Parse()
+
+	ctx := context.Background()
+	options := options.Client().ApplyURI(connectStr)
+	client, err := mongo.Connect(context.Background(), options)
+	if err != nil {
+		fmt.Println("Failed connect with mongoDB:", err)
+		log.Fatal(err)
+	}
+
+	if err = client.Ping(ctx, readpref.Primary()); err != nil {
+		fmt.Printf("Failed disConnect with mongoDB: %v", err)
+		log.Fatal(err)
+	}
+
+	defer func() {
+		if err := client.Disconnect(context.TODO()); err != nil {
+			fmt.Printf("Failed disConnect with mongoDB: %v", err)
+			log.Fatal(err)
+		}
+	}()
+
+	err = client.Database(dataBase).Collection(collection).FindOneAndUpdate(ctx, bson.D{{"state", "running"}}, bson.M{"$set": bson.M{"state": "finished"}}).Err()
+	if err != nil {
+		fmt.Println(err)
+	}
+	log.Print("Work done,state changed.")
+}
---
+++ e2e/images/mongodb/go.mod
@@ -0,0 +1,5 @@
+module github.com/carson/mongo
+
+go 1.15
+
+require go.mongodb.org/mongo-driver v1.4.4
