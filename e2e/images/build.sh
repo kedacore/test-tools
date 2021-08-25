7dfe600565e35a3098e8979b4b950465f86f9c38
Jorge Turrado Ferrero <Jorge_turrado@hotmail.es>
2021-08-25 10:10:59 +0200

    Adds RabbitMQ test image and tag all images with latest (#17)
    Co-authored-by: Zbynek Roubalik <726523+zroubalik@users.noreply.github.com>
    
    Signed-off-by: Jorge Turrado <jorge.turrado@docplanner.com>

--- e2e/images/build.sh
+++ e2e/images/build.sh
@@ -14,16 +14,17 @@ if [ "$1" == "--push" ]
 then
     for IMAGE_NAME in $(find * -name Dockerfile -exec dirname {} \; | tr '/' '-')
     do
-        docker push ghcr.io/kedacore/tests-$IMAGE_NAME:$IMAGE_TAG
-        docker push docker.io/kedacore/tests-$IMAGE_NAME:$IMAGE_TAG
+        docker image push -a ghcr.io/kedacore/tests-$IMAGE_NAME
+        docker image push -a docker.io/kedacore/tests-$IMAGE_NAME
     done
 else
     for IMAGE in $(find * -name Dockerfile)
     do
         IMAGE_NAME=$(dirname $IMAGE | tr '/' '-')
         pushd $(dirname $IMAGE)
-        docker build -t docker.io/kedacore/tests-$IMAGE_NAME:$IMAGE_TAG .
+        docker build -t docker.io/kedacore/tests-$IMAGE_NAME:$IMAGE_TAG -t docker.io/kedacore/tests-$IMAGE_NAME:latest .
         docker tag docker.io/kedacore/tests-$IMAGE_NAME:$IMAGE_TAG ghcr.io/kedacore/tests-$IMAGE_NAME:$IMAGE_TAG
+        docker tag docker.io/kedacore/tests-$IMAGE_NAME:$IMAGE_TAG ghcr.io/kedacore/tests-$IMAGE_NAME:latest
         popd
     done
 fi
