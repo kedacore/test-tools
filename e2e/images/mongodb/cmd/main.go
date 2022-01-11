de1257d3fd506c3075d7b0a979b44d65185a04c4
jorturfer <jorge_turrado@hotmail.es>
2022-01-11 22:05:22 +0100

    Update mongodb image
    Signed-off-by: jorturfer <jorge_turrado@hotmail.es>

--- e2e/images/mongodb/cmd/main.go
+++ e2e/images/mongodb/cmd/main.go
@@ -4,11 +4,12 @@ import (
 	"context"
 	"flag"
 	"fmt"
+	"log"
+
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/mongo/readpref"
-	"log"
 )
 
 var (
