ac6624ecada6cddbd1a76d96a17850920756fa19
Jorge Turrado Ferrero <Jorge_turrado@hotmail.es>
2022-04-04 11:28:45 +0200

    Remove dockerhub images generation (#62)
    Signed-off-by: Jorge Turrado <jorge_turrado@hotmail.es>

--- e2e/images/build.sh
+++ e2e/images/build.sh
@@ -15,16 +15,13 @@ then
     for IMAGE_NAME in $(find * -name Dockerfile -exec dirname {} \; | tr '/' '-')
     do
         docker image push -a ghcr.io/kedacore/tests-$IMAGE_NAME
-        docker image push -a docker.io/kedacore/tests-$IMAGE_NAME
     done
 else
     for IMAGE in $(find * -name Dockerfile)
     do
         IMAGE_NAME=$(dirname $IMAGE | tr '/' '-')
         pushd $(dirname $IMAGE)
-        docker build -t docker.io/kedacore/tests-$IMAGE_NAME:$IMAGE_TAG -t docker.io/kedacore/tests-$IMAGE_NAME:latest .
-        docker tag docker.io/kedacore/tests-$IMAGE_NAME:$IMAGE_TAG ghcr.io/kedacore/tests-$IMAGE_NAME:$IMAGE_TAG
-        docker tag docker.io/kedacore/tests-$IMAGE_NAME:$IMAGE_TAG ghcr.io/kedacore/tests-$IMAGE_NAME:latest
+        docker build -t ghcr.io/kedacore/tests-$IMAGE_NAME:$IMAGE_TAG -t ghcr.io/kedacore/tests-$IMAGE_NAME:latest .
         popd
     done
 fi
