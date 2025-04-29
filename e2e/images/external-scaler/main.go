5a439c817d0551cecee73164835d09fa3a2c299a
Jorge Turrado <jorge.turrado@mail.schwarz>
2025-04-30 00:25:12 +0200

    chore: Remove keep alive
    Signed-off-by: Jorge Turrado <jorge.turrado@mail.schwarz>

--- e2e/images/external-scaler/main.go
+++ e2e/images/external-scaler/main.go
@@ -10,7 +10,6 @@ import (
 	"time"
 
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/keepalive"
 
 	"github.com/gorilla/mux"
 	pb "github.com/kedacore/test-tools/external-scaler/externalscaler"
@@ -120,19 +119,7 @@ func main() {
 	defer cancel()
 	go RunManagementApi()
 
-	grpcServer := grpc.NewServer(
-		grpc.KeepaliveParams(keepalive.ServerParameters{
-			MaxConnectionIdle:     30 * time.Second, // If a client is idle for 15 seconds, send a GOAWAY
-			MaxConnectionAge:      30 * time.Second, // If any connection is alive for more than 30 seconds, send a GOAWAY
-			MaxConnectionAgeGrace: 5 * time.Second,  // Allow 5 seconds for pending RPCs to complete before forcibly closing connections
-			Time:                  5 * time.Second,  // Ping the client if it is idle for 5 seconds to ensure the connection is still active
-			Timeout:               1 * time.Second,  // Wait 1 second for the ping ack before assuming the connection is dead
-		}),
-		grpc.KeepaliveEnforcementPolicy(keepalive.EnforcementPolicy{
-			MinTime:             10 * time.Second,
-			PermitWithoutStream: true,
-		}),
-	)
+	grpcServer := grpc.NewServer()
 	lis, _ := net.Listen("tcp", ":6000")
 
 	pb.RegisterExternalScalerServer(grpcServer, &ExternalScaler{ctx: ctx})
