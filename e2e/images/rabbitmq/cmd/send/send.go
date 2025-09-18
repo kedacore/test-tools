983e3645af04f3f72468e2f7b50f997074630b4e
Wojciech Moczulski <wojciech.moczulski@stormlabs.eu>
2025-09-18 12:02:36 +0200

    allow for continuous publishing of messages in selected intervals (#226)
    Signed-off-by: Wojciech Moczulski <wojciech.moczulski@stormlabs.eu>

--- e2e/images/rabbitmq/cmd/send/send.go
+++ e2e/images/rabbitmq/cmd/send/send.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"os"
 	"strconv"
+	"time"
 
 	"github.com/streadway/amqp"
 )
@@ -17,42 +18,64 @@ func failOnError(err error, msg string) {
 
 func main() {
 	url := os.Args[1]
-	messageCount, err := strconv.Atoi(os.Args[2])
 	queueName := "hello"
-	if len(os.Args) == 4 {
+	interval := 0
+
+	messageCount, err := strconv.Atoi(os.Args[2])
+	failOnError(err, "Failed to parse second arg as messageCount : int")
+
+	if len(os.Args) >= 4 {
 		queueName = os.Args[3]
 	}
-	failOnError(err, "Failed to parse second arg as messageCount : int")
+
+	if len(os.Args) == 5 {
+		interval, err = strconv.Atoi(os.Args[4])
+	}
+	failOnError(err, "Failed to parse fourth arg as messages publishing interval (in seconds) : int")
+
 	conn, err := amqp.Dial(url)
 	failOnError(err, "Failed to connect to RabbitMQ")
 	defer conn.Close()
 
 	ch, err := conn.Channel()
 	failOnError(err, "Failed to open a channel")
+
 	defer ch.Close()
 
 	q, err := ch.QueueDeclare(
-		queueName, 	// name
-		false,   	// durable
-		false,   	// delete when unused
-		false,   	// exclusive
-		false,   	// no-wait
-		nil,     	// arguments
+		queueName, // name
+		false,     // durable
+		false,     // delete when unused
+		false,     // exclusive
+		false,     // no-wait
+		nil,       // arguments
 	)
 	failOnError(err, "Failed to declare a queue")
 
-	for i := 0; i < messageCount; i++ {
-		body := fmt.Sprintf("Hello World: %d", i)
-		err = ch.Publish(
-			"",     // exchange
-			q.Name, // routing key
-			false,  // mandatory
-			false,  // immediate
-			amqp.Publishing{
-				ContentType: "text/plain",
-				Body:        []byte(body),
-			})
-		log.Printf(" [x] Sent %s", body)
-		failOnError(err, "Failed to publish a message")
+	if interval > 0 {
+		log.Printf(" [*] Publishing %d message(s) per %d second(s). To exit press CTRL+C", messageCount, interval)
+	}
+
+	for {
+		for i := 0; i < messageCount; i++ {
+			body := fmt.Sprintf("Hello World: %d", i)
+			err = ch.Publish(
+				"",     // exchange
+				q.Name, // routing key
+				false,  // mandatory
+				false,  // immediate
+				amqp.Publishing{
+					ContentType: "text/plain",
+					Body:        []byte(body),
+				})
+			log.Printf(" [x] Sent %s", body)
+			failOnError(err, "Failed to publish a message")
+		}
+
+		if interval == 0 {
+			break
+		}
+
+		time.Sleep(time.Duration(interval) * time.Second)
 	}
 }
